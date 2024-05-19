eb930a6b4f3fe249d33b612b6ce11e501ad20af6
ruchi24072000 <ruchi.swarup12@gmail.com>
2024-05-19 18:00:15 +0530

    My Code Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TempratureConverterPipe } from './temprature-converter.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule,TempratureConverterPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
+
 })
 export class AppComponent {
-  title = 'PipeDemo';
+  
+ 
+   toDate: Date = new Date(); 
+   title: string = 'Angular pipes Example' ;
+   msg: string= 'Welcome to Angular';
+
+   num: number= 9542.14554;
+
+   per:number=.466;
+
+   cur: number= 175;
+
+   Fahrenheit:number=100;
+   celcius:number=98.4;
+   
 }
---
+++ src/app/temprature-converter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TempratureConverterPipe } from './temprature-converter.pipe';
+
+describe('TempratureConverterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TempratureConverterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
